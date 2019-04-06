9785cda3f217d2dbc06e4eea6b580e5ea8d69454
Kee <keejuan05@gmail.com>
2019-04-06 15:26:28 -0700

    Set per page as 24 for mobile view

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -49,7 +49,7 @@ class Listing < ApplicationRecord
     foreign_key: :listing_id,
     class_name: :Review
 
-  self.per_page = 25
+  self.per_page = 24
 
   def title_validation
     if title.length >= 255
