58cf08eaf72e0af03ddaede8cb88019dd5fd691a
Kee <keejuan05@gmail.com>
2019-04-06 17:07:16 -0700

    Change logo

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -34,7 +34,7 @@ class Header extends React.Component {
       <div id="global-header" className="header-nav-top-level">
         <header className="header-nav-inner">
           <div id="craftsy-logo" className="header-site-logo">
-            <Link to="/" aria-label="Craftsy" >Atsy </Link>
+            <Link to="/" aria-label="Craftsy" >Castle & Chair </Link>
           </div>
 
           <div className="header-search-div-wrapper">
