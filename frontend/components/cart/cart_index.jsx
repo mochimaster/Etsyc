790c6cfda66bee5f1015270a63dcdcc2d965bf6c
Kee <keejuan05@gmail.com>
2020-04-24 23:35:10 -0700

    refactor cart index complete

--- frontend/components/cart/cart_index.jsx
+++ frontend/components/cart/cart_index.jsx
@@ -1,108 +1,71 @@
-import React from 'react';
-import CartContainer from './cart_container';
-import CartIndexItem from './cart_index_item';
-import {asArray} from '../../reducers/selectors';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
-// import ListingIndexContainer from '../listing_form/listing_index_container';
+import CartIndexItem from './cart_index_item'
 
+const CartIndex = (props) => {
+  useEffect(() => {
+    props.getCarts(props.currentUserId)
+  }, [])
 
-class CartIndex extends React.Component {
-
-  constructor(props){
-    // debugger
-    super(props)
-    this.state = {
-      listings: props.listings ? props.listings : "",
-      carts: props.carts ? props.carts : ""
-      
-    }
+  if (!props.carts) {
+    return <div className="cart-empty">Your cart is empty.</div>
   }
 
-  componentDidMount(){
-    // debugger
-    this.props.getCarts(this.props.currentUserId).then(() => {
-      // debugger
-      this.setState({
-        carts: this.props.carts,
-        listings: this.props.listings
-      })
-    })
+  const itemCount = props.carts.length
 
-    // this.props.getListings()
-  }
-
-  // if (Object.keys(this.props.carts).length < 1){
-  //   return null
-  // }
-  // || Object.keys(this.props.entities.listings).length < 1
-
-  render() {
-
-    if(!this.props.carts ){
-      return <div className="cart-empty">Your cart is empty.</div>;
-    }
-
-    let sum=0;
-    for(let i=0; i< Object.keys(this.props.carts).length; i++ ){
-      sum += (this.props.carts[i].price * this.props.carts[i].quantity)
-    }
-
-    let itemCount=0;
-    itemCount=Object.keys(this.props.carts).length;
-
-    let itemCountDisplay;
-    if (itemCount < 1) {
-      itemCountDisplay=<p>Your cart is empty.</p>
-    } else if (itemCount === 1){
-      itemCountDisplay = <p>1 item in your cart</p>
-    } else {
-      itemCountDisplay = <p> {itemCount} items in your cart</p>
-    }
-
-    return (
-      <div className="cart-index-page-container">
-
-        <div className="sub-header-container">
-          <div className="cart-item-count">
-          {itemCountDisplay}
-          <Link className="keep-shopping-link btn btn-primary" to="/listings">Keep Shopping</Link>
-          </div>
-        </div>
-        <div className="cart-index-section-wrapper">
-          <ul className="cart-index-wrapper">
-            {this.props.carts.map(cart => {
-              return (<CartIndexItem key={cart.id} cart={cart}
-                  deleteCart={this.props.deleteCart} updateCart={this.props.updateCart} />)
-            })}
-
-          </ul>
-
-          <div className="cart-index-right-side">
-            <div className="cart-index-checkout">
-              <div className="total-amount">Item(s) total: ${sum}</div>
-
-              <button  className="checkout-button btn btn-primary"> Proceed to checkout (TBD)</button>
-            </div>
-
-
-
-          </div>
-        </div>
-
-      </div>
+  const itemCountDisplay =
+    itemCount === 1 ? (
+      <p>1 item in your cart</p>
+    ) : (
+      <p> {itemCount} items in your cart</p>
     )
-  }
+
+  let sum = 0
+  props.carts.forEach(
+    ({ price = 1, quantity = 0 }) => (sum += price * quantity)
+  )
+
+  return (
+    <div className="cart-index-page-container">
+      <div className="sub-header-container">
+        <div className="cart-item-count">
+          {itemCountDisplay}
+          <Link className="keep-shopping-link btn btn-primary" to="/listings">
+            Keep Shopping
+          </Link>
+        </div>
+      </div>
+      <div className="cart-index-section-wrapper">
+        <ul className="cart-index-wrapper">
+          {props.carts.map((cart) => {
+            return (
+              <CartIndexItem
+                key={cart.id}
+                cart={cart}
+                deleteCart={props.deleteCart}
+                updateCart={props.updateCart}
+              />
+            )
+          })}
+        </ul>
+
+        <div className="cart-index-right-side">
+          <div className="cart-index-checkout">
+            <div className="total-amount">Item(s) total: ${sum}</div>
+
+            <button className="checkout-button btn btn-primary">
+              <div class="tool-tip-text">
+                Online checkout is not currently available. Please call to
+                arrange for in-person pick up or delivery.
+              </div>
+              Proceed to checkout (TBD)
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-// listing={this.props.listings[cart.listing_id]}
-
-// return  cart.title
-// return <li> {cart.quantity} </li>
-
-// { this.props.carts.map(cart => {
-//   return (<CartIndexItem key={cart.id} cart={cart} listing={this.props.listing[cart.id]}/>)
-//
-// }) }
-
-export default CartIndex;
+export default CartIndex
