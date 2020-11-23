ac8ab0f5d4c1d87001d5999be27c1e03e4fd2970
Kee <keejuan05@gmail.com>
2020-11-23 13:08:43 -0800

    adjust venmo image

--- frontend/components/cart/cart_index.jsx
+++ frontend/components/cart/cart_index.jsx
@@ -54,7 +54,9 @@ const CartIndex = (props) => {
           <div className="cart-index-checkout">
             <div className="total-amount">Item(s) total: ${sum}</div>
 
-            <div className='payment-container'>
+            <div className="payment-container">
+              <p>Scan QR Code for payment</p>
+              <p>Venmo ID: @castleandchair</p>
               <a
                 href="https://venmo.com/code?user_id=1631646868570112774"
                 target="_blank"
