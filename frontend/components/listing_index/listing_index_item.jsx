f196ac10842dbe5a19b1b689648faa99ca8da842
Kee <keejuan05@gmail.com>
2021-02-08 00:15:42 -0800

    add tooltip

--- frontend/components/listing_index/listing_index_item.jsx
+++ frontend/components/listing_index/listing_index_item.jsx
@@ -26,40 +26,37 @@ const ListingIndexItem = ({ listing, deleteListing }) => {
 
   let displayImage
   if (listing.photoUrls) {
-    displayImage = <img src={listing.photoUrls[0]} />
+    displayImage = (
+      <img className="listing-thumbnail" src={listing.photoUrls[0]} />
+    )
   } else if (listing.photoUrl) {
-    displayImage = <img src={listing.photoUrl} />
+    displayImage = <img className="listing-thumbnail" src={listing.photoUrl} />
   }
+
+  const { id, title, author_id, merchant_name, price } = listing
 
   return (
     <li className="listing-item-wrapper">
       <div className="listing-item-image">
-        <Link to={`/listings/${listing.id}`}>
-          {/* <img src={listing.photoUrl} /> */}
-          {displayImage}
+        <Link to={`/listings/${id}`}>
+          <tooltip title={title}>{displayImage}</tooltip>
         </Link>
       </div>
       <div className="listing-item-title">
-        <tooltip title={listing.title}>
-          <Link
-            className="listing-item-title-link"
-            to={`/listings/${listing.id}`}
-          >
-            {listing.title}
+        <tooltip title={title}>
+          <Link className="listing-item-title-link" to={`/listings/${id}`}>
+            {title}
           </Link>
         </tooltip>
         <br />
       </div>
       <div className="listing-item-author">
-        <Link
-          className="listing-item-link"
-          to={`/users/${listing.author_id}/listings`}
-        >
-          {listing.merchant_name}
+        <Link className="listing-item-link" to={`/users/${author_id}/listings`}>
+          {merchant_name}
         </Link>
       </div>
       <div className="listing-item-review"></div>
-      <div className="listing-item-price">$ {listing.price}</div>
+      <div className="listing-item-price">$ {price}</div>
     </li>
   )
 }
