25d5dd186ed50684642491daf5f4fcb9e47326fa
Kee <keejuan05@gmail.com>
2022-04-17 14:17:53 -0700

    add class

--- frontend/components/header_banner/header_banner.jsx
+++ frontend/components/header_banner/header_banner.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const HeaderBanner = () => (
   <div
     id={`${
-      isMobile ? 'header-banner header-banner-mobile' : 'header-banner'
+      isMobile ? 'header-banner-mobile' : 'header-banner'
     }`}
   >
     CASTLE AND CHAIR OPEN BY APPOINTMENT.{' '}
