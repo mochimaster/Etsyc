53353750b8250ddf558e263bb7094683ef172d74
Kee <keejuan05@gmail.com>
2022-04-17 17:03:38 -0700

    add header banner animation

--- frontend/components/header_banner/header_banner.jsx
+++ frontend/components/header_banner/header_banner.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const HeaderBanner = () => (
-  <div id={`${window.isMobile ? 'header-banner-mobile' : 'header-banner'}`}>
+  <div
+    id={`${window.isMobile ? 'header-banner-mobile' : 'header-banner'}`}
+    className="animate__animated animate__bounce"
+  >
     CASTLE AND CHAIR OPEN BY APPOINTMENT.{' '}
     <Link className="appointment-link" to="/pages/appointment">
       BOOK NOW.
