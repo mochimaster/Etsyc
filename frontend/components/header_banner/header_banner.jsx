dd12199ee64d37ab6ffceebd8835c98df9122ad2
Kee <keejuan05@gmail.com>
2022-04-18 07:32:50 -0700

    randomize banner color

--- frontend/components/header_banner/header_banner.jsx
+++ frontend/components/header_banner/header_banner.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import { random } from 'lodash'
+
+const colors = ['#d68391', '#aec6cf']
+
 const HeaderBanner = () => (
   <div
     id={`${window.isMobile ? 'header-banner-mobile' : 'header-banner'}`}
     className="animate__animated animate__bounce"
+    style={{ background: colors[random(0, 1)] }}
   >
     CASTLE AND CHAIR OPEN BY APPOINTMENT.&nbsp;
     <Link className="appointment-link" to="/pages/appointment">
