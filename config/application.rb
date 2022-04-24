e2046918dad46cd78d0795bcfe14dfc186d4d110
Kee <keejuan05@gmail.com>
2022-04-24 10:30:35 -0700

    revert application rb initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,5 @@ module Craftsy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    
-    # config/application.rb
-    config.assets.initialize_on_precompile = false
   end
 end
