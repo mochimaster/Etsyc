be820dad6076d7a7a6dd2d284e04ba429f74a52b
Kee <keejuan05@gmail.com>
2022-05-16 12:18:47 -0700

    add sitemap

---
+++ frontend/sitemap-routes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Route } from 'react-router'
+
+export default (
+  <Route>
+    <Route path="/listings" />
+    <Route path="/faq" />
+    <Route path="/visit" />
+    <Route path="/listings/:id" />
+  </Route>
+)
