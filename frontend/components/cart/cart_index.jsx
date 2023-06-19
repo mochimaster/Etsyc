9a5aa5d497d4b3b6c5383d23997695d9c5cbff4b
Kee <keejuan05@gmail.com>
2023-06-19 10:01:17 -0700

    cart index page css mobile

--- frontend/components/cart/cart_index.jsx
+++ frontend/components/cart/cart_index.jsx
@@ -158,6 +158,7 @@ const CartIndex = (props) => {
 
   const displayCartItemCountAndKeepShopping = (
     <div className="cart-item-count">
+      <div> </div>
       {itemCountDisplay}
       <Link
         className={`${addMobileClassName(
@@ -167,6 +168,7 @@ const CartIndex = (props) => {
       >
         Keep Shopping
       </Link>
+      <div> </div>
     </div>
   )
 
