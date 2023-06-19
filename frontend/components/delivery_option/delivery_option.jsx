3ba609578bab53e11384c98db4b81f9503b3e12d
Kee <keejuan05@gmail.com>
2023-06-19 10:35:12 -0700

    add delivery option component

---
+++ frontend/components/delivery_option/delivery_option.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const DeliveryOption = () => (
+  <div className="">
+    Delivery available through Lugg.{' '}
+    <a
+      target="_blank"
+      href="https://lugg.com/estimate?origin=Z2lkOi8vbHVnZy9Mb2NhdGlvbi9kYjUxM2I5ZS03NmNmLTQxN2UtODllYy00NDBhZGI0YWEyODI"
+    >
+      Calculate delivery
+    </a>{' '}
+    to your address.
+  </div>
+)
+
+export default DeliveryOption
